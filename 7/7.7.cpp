cfa95480aaf433dd71547000d517c21fe84dd542
nikminer4sv <nikminer4sv@gmail.com>
2022-01-05 20:07:53 +0300

    7.7 prerelease

--- 7/7.7.cpp
+++ 7/7.7.cpp
@@ -2,8 +2,112 @@
 
 using namespace std;
 
+struct Node {
+
+    int Value;
+    Node* Left;
+    Node* Right;
+
+    Node() { Left = nullptr; Right = nullptr; }
+
+    Node(int Value) { Left = nullptr; Right = nullptr; this->Value = Value; }
+
+};
+
+class BinarySearchTree {
+private:
+    Node* Root;
+
+public:
+
+private:
+
+public:
+    BinarySearchTree() { Root = nullptr; }
+
+    void Add(int Value) {
+
+        Node* NewNode = new Node(Value);        
+
+        if (Root == nullptr) {
+
+            Root = NewNode;
+
+        } else {
+
+            Node* TempNode = Root;
+
+            while (true) {
+
+                if (TempNode->Value < NewNode->Value) {
+
+                    if (TempNode->Right != nullptr) {
+
+                        TempNode = TempNode->Right;
+
+                    } else {
+
+                        TempNode->Right = NewNode;
+                        break;
+
+                    }
+
+
+                } else {
+
+                    if (TempNode->Left != nullptr) {
+
+                        TempNode = TempNode->Left;
+
+                    } else {
+
+                        TempNode->Left = NewNode;
+                        break;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    void PrintImplementation(Node* TempNode) {
+
+        cout << TempNode->Value << " ";
+
+        if (TempNode->Left != nullptr) 
+            PrintImplementation(TempNode->Left);
+
+        if (TempNode->Right != nullptr) 
+            PrintImplementation(TempNode->Right);
+
+        
+
+    }
+
+    void Print() {
+
+        PrintImplementation(Root);
+
+    }
+
+    Node* GetRoot() { return Root; }
+};
+
 int main() {
 
-    
+    BinarySearchTree T;
+
+    T.Add(2);
+    T.Add(1);
+    T.Add(4);
+    T.Add(3);
+    T.Add(5);
+
+    T.Print();
+
 
 }
