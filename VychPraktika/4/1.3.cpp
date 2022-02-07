d286fc0c0b5591ad274f9b13b8be5f049833564d
HuKuTo3 <nvorlych@gmail.com>
2022-02-07 20:28:39 +0300

    commit name

--- VychPraktika/4/1.3.cpp
+++ VychPraktika/4/1.3.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int* Max(int Array[], int n, int k);
-int NumberOfDifferent(int Array[], int n);
-void Count(int Array[], int n, int *Numbers, int *Rate);
-void RateAndIndex(int Counter, int *Numbers, int *Rate, int &MaxIndex, int &MaxRate);
-int FirstInclusion(int Array[], int MaxIndex);
-void PrintRate(int Counter, int *Numbers, int *Rate);
-void Print(int Array[], int n);
+int& Max(int Array[], int n, int k);
 
 int main()
 {
@@ -21,147 +14,43 @@ int main()
     cout << "Enter the value of k: ";
     cin >> k;
 
-    int* p = Max(Array, n, k);
-    *p = k;
-
-    Print(Array, n);
+    int& p = Max(Array, n, k);
+    p = k;
 }
 
-int* Max(int Array[], int n, int k)
+int& Max(int Array[], int n, int k)
 {
     for (int i = 1; i <= n; i++)
     {
         Array[i - 1] = rand() % 10 + 1;
     }
 
-    int Counter = NumberOfDifferent(Array, n);
-    int Numbers[Counter];
-    int Rate[Counter];
+    int MaxIndex = 0, MaxCount = 0;
 
-    Count(Array, n, Numbers, Rate);
-
-    int MaxIndex = 0;
-    int MaxRate = 0;
-
-    RateAndIndex(Counter, Numbers, Rate, MaxIndex, MaxRate);
-
-    int First = FirstInclusion(Array, MaxIndex);
-
-    PrintRate(Counter, Numbers, Rate);
-
-    return &Array[First];
-}
-
-int NumberOfDifferent(int Array[], int n)
-{
-    int Counter = 0;
-
-    for (int i = 0; i < n; i++)
+    for (int Index = 0; Index < n; Index++)
     {
-        bool Flag = true;
+        int CurrentCount = 1;
 
-        for (int Before = 0; Before < i; Before++)
+        for (int Index_2 = Index + 1; Index_2 < n; Index_2++)
         {
-            if (Array[Before] == Array[i])
+            if (Array[Index_2] == Array[Index])
             {
-                Flag = false;
+                CurrentCount++;
             }
         }
-        if (Flag)
+        if (CurrentCount > MaxCount)
         {
-            Counter++;
+            MaxIndex = Index;
+            MaxCount = CurrentCount;
+        }
+        else if ((CurrentCount == MaxCount) && (Array[MaxIndex] > Array[Index]))
+        {
+            MaxIndex = Index;
+            MaxCount = CurrentCount;
         }
     }
 
-    return Counter;
-}
+    cout << "MaxCount: " << MaxCount << "\tMaxIndex: " << MaxIndex << endl;
 
-void Count(int Array[], int n, int *Numbers, int *Rate)
-{
-    int Chislo = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        int RateOfNumber = 1;
-        bool Flag = true;
-        
-        for (int After = i + 1; After < n; After++)
-        {
-            if (Array[i] == Array[After])
-            {
-                RateOfNumber++;
-            }
-        }
-        for (int Before = 0; Before < i; Before++)
-        {
-            if (Array[Before] == Array[i])
-            {
-                Flag = false;
-            }
-        }
-        if (Flag)
-        {
-            Numbers[Chislo] = Array[i];
-            Rate[Chislo++] = RateOfNumber;
-        }
-    }
-}
-
-void RateAndIndex(int Counter, int *Numbers, int *Rate, int &MaxIndex, int &MaxRate)
-{
-    for (int i = 0; i < Counter; i++)
-    {
-        if (Rate[i] > MaxRate)
-        {
-            MaxRate = Rate[i];
-            MaxIndex = i;
-        }
-        else if (Rate[i] == MaxRate)
-        {
-            if (Numbers[i] < Numbers[MaxIndex])
-            {
-                MaxRate = Rate[i];
-                MaxIndex = i;
-            }
-        }
-    }
-}
-
-int FirstInclusion(int Array[], int MaxIndex)
-{
-    int First = 0;
-
-    while (Array[First] != Array[MaxIndex])
-    {
-        First++;
-    }
-
-    return First;
-}
-
-void PrintRate(int Counter, int *Numbers, int *Rate)
-{
-    cout << "\n\nRate of numbers:\n\n";
-
-    for (int i = 0; i < Counter; i++)
-    {
-        cout << "  " << Numbers[i] << " - " << Rate[i] << endl;
-    }
-
-    cout << "\n\n";
-}
-
-void Print(int Array[], int n)
-{
-    cout << "Numbers:\n\n";
-
-    for (int i = 1; i <= n; i++)
-    {
-        cout << Array[i - 1] << " ";
-
-        if ((i % 20) == 0)
-        {
-            cout << endl;
-        }
-    }
+    return Array[MaxIndex];
 }
