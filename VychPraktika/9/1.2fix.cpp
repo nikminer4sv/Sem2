3b6de5e45aca77115d28396b5be58491f9a78c08
nikminer4sv <nikminer4sv@gmail.com>
2022-02-16 01:16:34 +0300

    Gg

--- VychPraktika/9/1.2fix.cpp
+++ VychPraktika/9/1.2fix.cpp
@@ -3,110 +3,61 @@
 
 using namespace std;
 
-bool WordInText(char* text, char* word, int wordLength) {
-
-    if (strlen(text) < wordLength)
-        return false;
-
-    for (int i = 0; i < strlen(text) - wordLength + 1;i++) {
-
-        int counter = 0;
-
-        for (int j = 0; j < wordLength; j++) {
-
-            if (word[j] == text[i + j]) {
-                counter += 1;
-            }
-
-        }
-
-        if (counter == wordLength) {
-            return true;
-        }
-
-    }
-
-    return false;
-
+bool WordInText(const char* text, const char* word) {
+    return strstr(text, word) != nullptr;
 }
 
 bool IsSeparator(char symbol) {
-
-    char array[] = ".,:;!?-() ";
-
-    for (char separator : array) {
-
-        if (separator == symbol)
-            return true;
-
-    }
-
-    return false;
-
+    static const char array[] = ".,:;!?-() ";
+    return strchr(array, symbol) != nullptr;
 }
 
-tuple<char*, int> EditText(char* text) {
-
+char* EditText(char* text) {
     char* result = new char[256];
     int resultIndex = 0;
 
     int index = -1;
     bool isWord = false;
 
-    for (int i = 0; i < strlen(text); i++) {
-
+    int len = strlen(text);
+    for (int i = 0; i < len; i++) {
         if (IsSeparator(text[i])) {
-
             if (isWord == true) {
-
                 isWord = false;
                 
-                char* word = new char[i - index];
-                
+                char* word = new char[i - index]{};
                 for (int j = 0; j < i - index; j++) {
                     word[j] = text[j + index];
                 }
 
-                if (!WordInText(result, word, i - index)) {
+                if (!WordInText(result, word)) {
                     for (int j = 0; j < i - index; j++) {
                         result[resultIndex++] = word[j];
                     }
+                } 
 
-                }         
-
+                delete[] word;        
             }
 
             result[resultIndex++] = text[i];
 
         } else {
-
             if (isWord == false) {
                 index = i;
                 isWord = true;
             }
-
         }
-
     }
 
-    return make_tuple(result, resultIndex);
-
+    return result;
 }
 
 int main() {
-
-    char* text = new char[256];
+    char* text = new char[256]{};
     gets(text);
-    
-    char* result;
-    int resultSize;
 
-    tie(result, resultSize) = EditText(text);
+    auto result = EditText(text);
+    cout << result;
 
-    for (int i = 0; i < resultSize + 1; i++) {
-
-        cout << result[i];
-                
-    }
-
+    delete[] result;
 }
