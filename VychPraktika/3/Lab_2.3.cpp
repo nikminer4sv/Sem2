7d6cdb41136436a9418aac1c8ca2972c9562d954
dimon58331 <ifrez5833@gmail.com>
2022-02-21 02:27:37 +0300

    change Lab_2.3.cpp

--- VychPraktika/3/Lab_2.3.cpp
+++ VychPraktika/3/Lab_2.3.cpp
@@ -39,20 +39,27 @@ struct Node {
     }
 };
 
-struct list {
+
+class list{
+private:
     Node* first;
     Node* last;
-
     int size = 0;
 
-    list() : first(nullptr), last(nullptr) {}
+    string Surname;
+    string Name;
+    string Patronymic;
+    int Day;
+    int Mounth;
+    int Year;
+    int Course;
+    float Progress;
 
     bool is_empty() {
         return first == nullptr;
-    }    
-
-    void createNewList(list& newL, char firstSurnameSymbol, char firstNameSymbol, char firstPatronymicSymbol){
-        
+    }   
+    
+    void createNewList(list& newL, char firstSurnameSymbol, char firstNameSymbol, char firstPatronymicSymbol){    
         while (true)
         {
             if (is_empty()) return;
@@ -91,7 +98,19 @@ struct list {
             slow->next = fast->next;
             delete fast;
         }
-         
+    }
+
+    void print() {
+        if (is_empty()) return;
+        Node* p = first;
+        while (p) {
+            cout << p->student.Surname << " " << p->student.Name << " " << p->student.Patronymic << endl;
+            cout << p->student.Data.Day << '.' << p->student.Data.Mounth << '.' << p->student.Data.Year << endl;
+            cout << "Course - " << p->student.Course << endl;
+            cout << "Progress - " << p->student.Progress << endl << endl;
+            p = p->next;
+        }
+        cout << endl;
     }
 
     void push_back(string surname, string name, string patronymic, int day, int mounth, int year, int course, float progress) {
@@ -119,22 +138,7 @@ struct list {
         last->next = p; // присваиваем в текущем последнем узле указателю next следующий узел 
         last = p; // last присваем новый последний узел
         size++;
-
     }
-
-    void print() {
-        if (is_empty()) return;
-        Node* p = first;
-        while (p) {
-            cout << p->student.Surname << " " << p->student.Name << " " << p->student.Patronymic << endl;
-            cout << p->student.Data.Day << '.' << p->student.Data.Mounth << '.' << p->student.Data.Year << endl;
-            cout << "Course - " << p->student.Course << endl;
-            cout << "Progress - " << p->student.Progress << endl << endl;
-            p = p->next;
-        }
-        cout << endl;
-    }
-
 
     // Node* find(string _val) {
     //     Node* p = first;
@@ -162,16 +166,14 @@ struct list {
         last = p;
     }
 
-    void remove(char firstSurnameSymbol, char firstNameSymbol, char firstPatronymicSymbol) {
+    void remove(string Surname, string Name, string Patronymic) {
         if (is_empty()) return;
-        if (first->student.Name[0] == firstNameSymbol && first->student.Surname[0] == firstSurnameSymbol
-            && first->student.Patronymic[0] == firstPatronymicSymbol) 
+        if (first->student.Surname == Surname && first->student.Name == Name && first->student.Patronymic == Patronymic) 
         {
             remove_first();
             return;
         }
-        else if (last->student.Name[0] == firstNameSymbol && last->student.Surname[0] == firstSurnameSymbol
-                && last->student.Patronymic[0] == firstPatronymicSymbol) 
+        else if (last->student.Surname == Surname && last->student.Name == Name && last->student.Patronymic == Patronymic) 
         {
             remove_last();
             return;
@@ -179,8 +181,8 @@ struct list {
 
         Node* slow = first;
         Node* fast = first->next;
-        while (fast && fast->student.Name[0] != firstNameSymbol && fast->student.Surname[0] != firstSurnameSymbol
-            && fast->student.Patronymic[0] != firstPatronymicSymbol) {
+        while (fast && (fast->student.Surname != Surname && fast->student.Name != Name && fast->student.Patronymic != Patronymic)) 
+        {
             fast = fast->next;
             slow = slow->next;
         }
@@ -193,28 +195,112 @@ struct list {
         delete fast;
     }
 
+public:
+    list() : first(nullptr), last(nullptr) {}
+
+    void Menu(){
+
+        list newList;
+        while (true)
+        {   
+            cout << "Enter the choose:  " << endl;
+
+            cout << "1. Add new student." << endl;
+            cout << "2. Search a student by \"symbols\" and create a new list of them." << endl;
+            cout << "3. Delete a student." << endl;
+            cout << "4. Print the list's of students." << endl;
+            cout << "5. Exit." << endl;
+
+            int choose;
+            cin >> choose;
+
+            if(choose == 5) break; 
+
+            switch (choose)
+            {
+                case 1:
+                {
+                    cout << "Surname = ";
+                    cin >> Surname;
+
+                    cout << "Name = ";
+                    cin >> Name;
+
+                    cout << "Patronymic = ";
+                    cin >> Patronymic;
+
+                    cout << "Data of birth (1 4 2002):";
+                    cin >> Day;
+                    cin >> Mounth;
+                    cin >> Year;
+
+                    cout << "Course of student: ";
+                    cin >> Course;
+
+                    cout << "Progress of student: ";
+                    cin >> Progress;
+                    
+                    push_back(Surname, Name, Patronymic, Day, Mounth, Year, Course, Progress);
+                    
+                    break;
+                }
+                case 2:
+                {
+                    cout << "Enter the first symbols of surname, name, patronymic (\"K\" \"D\" \"A\"): ";
+                    char firstSurnameSymbol;
+                    cin >> firstSurnameSymbol;
+
+                    char firstNameSymbol;
+                    cin >> firstNameSymbol;
+
+                    char firstPatronymicSymbol;
+                    cin >> firstPatronymicSymbol;
+
+                    createNewList(newList, firstSurnameSymbol, firstNameSymbol, firstPatronymicSymbol);
+
+                    break;
+                }
+                case 3:
+                {
+                    cout << "Enter the surname, name, patronymic of student (\"Kazlou\" \"Dzmitry\" \"Andreevich\")";
+
+                    cout << "Surname = ";
+                    cin >> Surname;
+
+                    cout << "Name = ";
+                    cin >> Name;
+
+                    cout << "Patronymic = ";
+                    cin >> Patronymic;
+
+                    remove(Surname, Name, Patronymic);
+
+                    break;
+                }
+                case 4:
+                {   
+                    cout << "\n========================LIST=OF=STUDENTS===========================" << endl;
+                    print();
+                    cout << "====================SECOND=LIST=OF=STUDENTS========================" << endl;
+                    newList.print();
+
+                    break;
+                }
+
+                default:
+                    break;
+            }
+
+            cout << endl;
+        } 
+    }
 };
+
 
 int main(){
 
-    list l;
-    list newL;
-    //cout << l.is_empty() << endl;
-
-    l.push_back("Kazlou", "Dzmitry", "Andreevich", 4, 5, 2004, 1, 7.0);
-    l.push_back("Keglik", "Mikhail", "Andreevich", 15, 9, 2011, 1, 5.4);
-    l.push_back("Masherov", "Maks", "Andreevich", 11, 12, 2000, 1, 5.4);
-    l.push_back("Mazurov", "Malkin", "Andreevich", 9, 2, 1941, 1, 5.4);
-
-    //cout << l.is_empty() << endl;
-    cout << "=====================" << endl;
-    l.print();
-    cout << "=====================" << endl;
-    l.createNewList(newL, 'M', 'M', 'A');
-    //l.remove('M', 'K', 'A');
-    l.print();
-    cout << "=====================" << endl;
-    newL.print();
+    list Students;
+    Students.Menu();
 
     return 0;
 }
