45b1ca99ccc9aedb6b455c691d4afc70d33bf51d
nikminer4sv <nikminer4sv@gmail.com>
2022-02-23 11:15:28 +0300

    GG

--- VychPraktika/9/2.5fix.cpp
+++ VychPraktika/9/2.5fix.cpp
@@ -1,90 +1,72 @@
+#include <array>
 #include <iostream>
 
 using namespace std;
 
+template<size_t Capacity>
 class ArrayList {
 private:
-    char* components = new char[256];
-    int* indices = new int[256];
-    int startIndex = 0;
-    int endIndex = 0;
+    static constexpr size_t _capacity = 256U;
 
+    std::array<char, Capacity> _data{};
+    std::array<size_t, Capacity> _index{};
+
+    int _idx_head = 0;
+    int _idx_tail = 0;
+
+    size_t _size = 0U;
 public:
 
 private:
     struct InvalidIndex : public exception{
-
-        const char * what () const throw () {
+        [[nodiscard]] const char * what () const noexcept override {
             return "Index must be right:)";
         }
-
     };
 
 public:
 
-    char Get(int index) {
-
-        if (index < 0 || index > GetSize() - 1) 
+    char Get(int index) const {
+        if (index < 0 || index > GetSize() - 1)
             throw InvalidIndex();
 
-        int tempIndex = startIndex;
+        int it = _idx_head;
+        for (size_t i = 0; i < index; i++ )
+            it = _index[it];
 
-        for (int i = 0; i < index; i++ )
-            tempIndex = indices[tempIndex];
-
-        return components[tempIndex];
-
+        return _data[it];
     }
 
-    int GetSize() {
-
-        if (endIndex == startIndex)
-            return 0;
-
-        int index = startIndex;
-        int counter = 0;
-
-        while (indices[index] != 0) {
-
-            counter += 1;
-            index = indices[index];
-
-        }
-
-        return counter + 1;
-
+    [[nodiscard]] size_t GetSize() const{
+        return _size;
     }
 
-    void PrintRawData() {
-
-        for (int i = 0; i < endIndex; i++)
-            cout << components[i] << " ";
-
+    void PrintRawData() const {
+        cout << "DATA: ";
+        for (int i = 0; i < _idx_tail; i++)
+            cout << _data[i] << " ";
         cout << endl;
 
-        for (int i = 0; i < endIndex; i++)
-            cout << indices[i] << " ";
-
+        cout << "INDX: ";
+        for (int i = 0; i < _idx_tail; i++)
+            cout << _index[i] << " ";
         cout << endl;
-
     }
 
     void Delete(int index) {
-
-        if (index < 0 || index > GetSize() - 1) 
+        if (index < 0 || index > GetSize() - 1)
             throw InvalidIndex();
-        
 
         if (index == 0) {
-            startIndex = indices[startIndex];
+            _idx_head = _index[_idx_head];
             return;
         }
 
-        int tempValue = startIndex;
-        for (int i = 0; i < index - 1; i++) 
-            tempValue = indices[tempValue];
+        int tempValue = _idx_head;
+        for (int i = 0; i < index - 1; i++)
+            tempValue = _index[tempValue];
 
-        indices[tempValue] = indices[indices[tempValue]];
+        _index[tempValue] = _index[_index[tempValue]];
 
         // H e l g o i
         // 5 2 3 4 0 1
@@ -93,88 +75,112 @@ public:
 
     }
 
-    void Append(char value) {
+    void Delete(size_t idx, size_t length){
+        if (idx < 0 || idx+length > GetSize() - 1)
+            throw InvalidIndex();
 
-        if (endIndex == 0) {
-            components[endIndex++] = value;
+        for(int it = idx; it<idx+length;it++){
+            Delete(it);
+        }
+    }
+
+    bool Append(char value) {
+        if (_size == _capacity) {
+            return false;
+        }
+
+        if (_idx_tail == 0) {
+            _data[_idx_tail++] = value;
+            return true;
+        }
+
+        _data[_idx_tail] = value;
+        _index[_idx_tail - 1] = _idx_tail;
+        _idx_tail += 1;
+
+        _size++;
+        return true;
+    }
+
+    bool Insert(char value, int index) {
+        if (_size == _capacity) {
+            return false;
+        }
+
+        if (index < 0 || index > GetSize() - 1)
+            throw InvalidIndex();
+
+        _data[_idx_tail] = value;
+        _index[index - 1] = _idx_tail;
+        _index[_idx_tail] = index;
+        _idx_tail += 1;
+
+        _size++;
+        return true;
+    }
+
+    int Contains(char value) {
+        if (!_size) {
+            return -1;
+        }
+
+        size_t it = _idx_head;
+        while (_index[it]) {
+            if (_data[it] == value) {
+                return it;
+            }
+            it = _index[it];
+        }
+
+        return _data[it] == value ? it : -1;
+    }
+
+    int Contains(const ArrayList<Capacity>& other) {
+        int pos_first = -1;
+        for(int idx = 0; idx < other.GetSize(); idx++){
+            int pos_current = Contains(other.Get(idx));
+            if(pos_current == -1){
+                return -1;
+            }
+            if(pos_first == -1){
+                pos_first = pos_current;
+            }
+        }
+
+        return pos_first;
+    }
+
+    void Print() const {
+        if(!_size){
             return;
         }
 
-        components[endIndex] = value;
-        indices[endIndex - 1] = endIndex;
-        endIndex += 1;
-
-
-    }
-
-    void Insert(char value, int index) {
-
-        if (index < 0 || index > GetSize() - 1) 
-            throw InvalidIndex();
-
-        components[endIndex] = value;
-        indices[index - 1] = endIndex;
-        indices[endIndex] = index;
-        endIndex += 1;
-
-    }
-
-    void Print() {
-
-        int index = startIndex;
-
-        while (indices[index] != 0) {
-
-            cout << components[index] << " ";
-            index = indices[index];
-
+        size_t it = _idx_head;
+        while (_index[it]) {
+            cout << _data[it] << " ";
+            it = _index[it];
         }
 
-        cout << components[index];
-
-        cout << endl;
-
+        cout << _data[it] << endl;
     }
 };
 
-void Task(ArrayList& list1, ArrayList& list2) {
+template<size_t Capacity>
+void Task(ArrayList<Capacity>& list1, ArrayList<Capacity>& list2) {
 
-    if (list2.GetSize() == 0 || list1.GetSize() - list2.GetSize() < 0) 
+    if (list2.GetSize() == 0 || list1.GetSize() - list2.GetSize() < 0)
         return;
 
-    int forSize = list1.GetSize() - list2.GetSize();
-
-    for (int i = 0; i < forSize; i++) {
-
-        bool exist = true;
-
-        for (int j = 0; j < list2.GetSize(); j++) {
-
-            if (list1.Get(i + j) != list2.Get(j)) {
-
-                exist = false;
-                break;
-
-            }
-
-        }
-
-        if (exist) {
-            
-            for (int j = 0; j < list2.GetSize(); j++) {
-                list1.Delete(i);
-                cout << i << endl;
-            }
-
-        }
-
+    auto pos = list1.Contains(list2);
+    if(pos >= 0){
+        list1.Delete(pos, list2.GetSize());
     }
 }
 
 
 int main() {
 
-    ArrayList list1;
+    ArrayList<16> list1;
     list1.Append('H');
     list1.Append('e');
     list1.Append('l');
@@ -182,33 +188,11 @@ int main() {
     list1.Append('o');
     list1.Print();
 
-    ArrayList list2;
+    ArrayList<16> list2;
     list2.Append('e');
     list2.Append('l');
+    list2.Print();
 
     Task(list1, list2);
-    /*list1.Delete(1);
-    list1.Delete(1);
-    list1.Delete(1);
-    list1.Delete(1);
-    list1.Print();*/
-
-    // 1 3 6 9
-    // 1 3 3 0
-
-    /*for (int i = 0; i < SIZE; i++) 
-        cout << components[i] << " ";
-    cout << endl;
-    for (int i = 0; i < SIZE; i++) 
-        cout << indices[i] << " ";
-    cout << endl;*/
-
-    //Print();
-
-    // H e l l o i 
-    // 5 2 3 4 0 1
-
-    //list1.PrintRawData();
     list1.Print();
-
 }
